499585fd6c3a6e32d06b91b85313fbc52b98df24
Amish Shah <amishshah.2k@gmail.com>
2016-08-09 21:30:32 +0100

    word changes to cover every letter every 9 rounds at least once

--- src/quotes.js
+++ src/quotes.js
@@ -1,12 +1,5 @@
-var words = [
-    "apple", "cat", "dog", "fish", "hack", "code", "braille", "sky", "pi",
-    "learn", "zoo", "why", "box", "rat",
-];
-
-function rand() {
-    return words[Math.floor(Math.random() * words.length)];
-}
+var words = require("./words.js");
 
 exports.gen = function() {
-    return rand() + " " + rand() + " " + rand();
+    return [words.gen(), words.gen(), words.gen()].join(" ");
 }
